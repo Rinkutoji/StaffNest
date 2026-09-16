c7b750e6c8f99a055b7d1d4713eb96376143ee44
Rinkutoji <rinkutojipro@gmail.com>
2026-09-16 21:43:15 +0700

    Add TImeout

--- backend/config/database.php
+++ backend/config/database.php
@@ -22,6 +22,7 @@ function getDBConnection()
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_TIMEOUT            => 5,
         ];
         $db = new PDO($dsn, DB_USER, DB_PASS, $options);
 
